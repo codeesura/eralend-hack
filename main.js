61334afbe6aa6964598b6dff14f8d042973245bf
codeesura <120671243+codeesura@users.noreply.github.com>
2023-07-25 15:46:08 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,123 @@
+const ethers = require('ethers');
+const fs = require('fs');
+const provider = new ethers.providers.JsonRpcProvider('https://zksync-era.rpc.thirdweb.com');
+const wallet = new ethers.Wallet("private_key",provider);
+
+const eralend_contractAddress = "0x1BbD33384869b30A323e15868Ce46013C82B86FB";
+const eralend_poolAbi = [
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "borrowAmount",
+          "type": "uint256"
+        }
+      ],
+      "name": "borrow",
+      "outputs": [
+        {
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [],
+      "name": "mint",
+      "outputs": [
+          {
+              "name": "",
+              "type": "uint256"
+          }
+      ],
+      "payable": true,
+      "stateMutability": "payable",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
+          {
+              "name": "redeemAmount",
+              "type": "uint256"
+          }
+      ],
+      "name": "redeemUnderlying",
+      "outputs": [
+          {
+              "name": "",
+              "type": "uint256"
+          }
+      ],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [
+      {
+        "name": "owner",
+        "type": "address"
+      }
+    ],
+    "name": "balanceOf",
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  }
+];
+
+async function eralend_borrow() {
+    const contract = new ethers.Contract(eralend_contractAddress, eralend_poolAbi, wallet);
+    const balanceResult = await contract.balanceOf(wallet.address);
+    const balance = ethers.BigNumber.from(balanceResult).mul(200000000);
+    const GasPrice = await provider.getGasPrice();
+    const min = 1400000;
+    const max = 1500000;
+  
+    const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+    const randomNumberString = randomNumber.toString();
+  
+    let response;
+    let retries = 20;
+    while(retries-- > 0){
+        try{
+            response = await contract.estimateGas.redeemUnderlying(
+              balance,
+              {
+                  gasLimit: ethers.BigNumber.from(randomNumberString),
+                  gasPrice: GasPrice,
+              }
+            );
+  
+            response = await contract.redeemUnderlying(
+              balance,
+              {
+                  gasLimit: ethers.BigNumber.from(randomNumberString),
+                  gasPrice: GasPrice,
+              }
+            );
+            await response.wait();
+            break;
+        } catch(error){
+            if(retries == 0) {
+                return false;
+            } else {
+                console.log("\x1b[33m[%s | %s] [RETRY] Error ..\x1b[0m", new Date().toLocaleString(), shortAddress);
+            }
+        }
+    }
+}
+
+eralend_borrow();
